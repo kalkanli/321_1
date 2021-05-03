7e7fe3f62fe10c8fc32417c93315b1e4f1d0d2ec
ramazanbulut <56506578+ramazanbulut@users.noreply.github.com>
2021-05-03 22:03:07 +0300

    Update q15.sql

--- q15.sql
+++ q15.sql
@@ -7,3 +7,4 @@ ORDER By short_name) JOIN Drivers ON driver_id=driver_one )  JOIN Drivers ON dri
 --After getting teams table, by using driver_one and driver_two fields, we get their total podiums.
 -- Then, by checking driver one's total_podiums > driver two's total_podiums, we filter teams according to this condition.
 -- Then we make drivers join to these filtered Team table we get their total_podiums. We make this process one by one in 
+-- other words firstly we add driver_one's total_podium column then we get two's column 
