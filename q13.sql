5fcccbaef4b9014a1c64f32822a21c8ac7bb83d9
ramazanbulut <56506578+ramazanbulut@users.noreply.github.com>
2021-05-03 21:25:57 +0300

    Update q13.sql
    q13 has changed and been commented

--- q13.sql
+++ q13.sql
@@ -1,5 +1,8 @@
-SELECT * FROM (SELECT   Tracks.circuit_type, Teams.short_name, count(lap_recorder)  as lap_recorder2 FROM Teams,Tracks WHERE ( Teams.driver_one= Tracks.lap_recorder OR  Teams.driver_two=Tracks.lap_recorder ) AND Tracks.circuit_type is 'street' GROUP by Teams.team_id , Tracks.circuit_type)  WHERE lap_recorder2 IN
-( SELECT  max(lap_recorder1) FROM (SELECT    Tracks.circuit_type, Teams.short_name, count(lap_recorder)  as lap_recorder1 FROM Teams,Tracks WHERE ( Teams.driver_one= Tracks.lap_recorder OR  Teams.driver_two=Tracks.lap_recorder ) AND Tracks.circuit_type is 'street' GROUP by Teams.team_id , Tracks.circuit_type ORDER BY circuit_type,lap_recorder1  DESC) )
-UNION
-SELECT * FROM (SELECT   Tracks.circuit_type, Teams.short_name, count(lap_recorder)  as lap_recorder2 FROM Teams,Tracks WHERE ( Teams.driver_one= Tracks.lap_recorder OR  Teams.driver_two=Tracks.lap_recorder ) AND Tracks.circuit_type is 'race' GROUP by Teams.team_id , Tracks.circuit_type)  WHERE lap_recorder2 IN
-( SELECT  max(lap_recorder1) FROM (SELECT    Tracks.circuit_type, Teams.short_name, count(lap_recorder)  as lap_recorder1 FROM Teams,Tracks WHERE ( Teams.driver_one= Tracks.lap_recorder OR  Teams.driver_two=Tracks.lap_recorder ) AND Tracks.circuit_type is 'race' GROUP by Teams.team_id , Tracks.circuit_type ORDER BY circuit_type,lap_recorder1  DESC) )
+SELECT  circuit_type,short_name,team_lap_recorder FROM (SELECT    Tracks.circuit_type, Teams.short_name, count(lap_recorder)  as team_lap_recorder FROM Teams,Tracks WHERE ( Teams.driver_one= Tracks.lap_recorder OR  Teams.driver_two=Tracks.lap_recorder )  GROUP by Teams.short_name ,   Tracks.circuit_type   )
+WHERE circuit_type|| '-' || team_lap_recorder in
+ (SELECT circuit_type|| '-' ||max(team_lap_recorder) FROM
+(SELECT  short_name, circuit_type,team_lap_recorder FROM (SELECT    Tracks.circuit_type, Teams.short_name, count(lap_recorder)  as team_lap_recorder FROM Teams,Tracks WHERE ( Teams.driver_one= Tracks.lap_recorder OR  Teams.driver_two=Tracks.lap_recorder )  GROUP by Teams.short_name ,   Tracks.circuit_type   ) )
+ GROUP by circuit_type) ORDER BY circuit_type,team_lap_recorder  DESC
+
+ -- Firstly, we ensure conditions and find team_lap_recorder s , then we calculate max value of team_lap_recorder. After that we can find rows that have team_lap_recorder with the same value as max has.
+-- While ensuring conditions we can find by checking track's lap_recorder value is team's driver_one or driver_two with or logical operator. Also we should group results by circuit type. When we count tracks lap_recorders with same value with team's member we get team's lap records.
